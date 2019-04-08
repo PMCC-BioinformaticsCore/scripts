364895ba82311e7d2604b4d47b77e971122eb1a7
Jiaan Yu <jiaan.yu@petermac.org>
2019-04-08 13:56:44 +1000

    Add strong somatic filter on the vardict vcfs

--- vcf_utils/normalisedvcf.py
+++ vcf_utils/normalisedvcf.py
@@ -171,14 +171,24 @@ class NormalisedVcf:
                      .format(self.name))
 
         # Continue to read the file, this time the variants
-        for line in vcf:
-            variant = Variant().process_somatic_variant(
-                      line, self.caller, normal_index, tumor_index)
-            if self.caller == 'vardict' and (variant.info['STATUS'] ==
-                                             'Germline'):
-                # Skip the germline variants called in vardict
-                pass
-            else:
+        # The vardict vcf needs extra filtering of strong somatic variants
+        if self.caller.lower() == 'vardict':
+            for line in vcf:
+                variant = Variant().process_somatic_variant(
+                          line, self.caller, normal_index, tumor_index)
+                if variant.info['STATUS'] == 'StrongSomatic':
+                    cleaned_variant = Variant.select_info(variant, info_cols,
+                                                          format_cols,
+                                                          caller=self.caller,
+                                                          somatic=True)
+                    self.variants.update({cleaned_variant.variant_key:
+                                          cleaned_variant})
+                else:
+                    pass
+        else:
+            for line in vcf:
+                variant = Variant().process_somatic_variant(
+                        line, self.caller, normal_index, tumor_index)
                 cleaned_variant = Variant.select_info(variant,
                                                       info_cols, format_cols,
                                                       caller=self.caller,
