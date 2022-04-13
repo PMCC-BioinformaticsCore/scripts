da9fdbabff4a1e4d7e40cdc9d2e4574c17a2cf47
Jiaan Yu <jiaan7.yu@gmail.com>
2022-04-13 16:20:07 +1000

    Add instructions for RNA-seq related scripts

--- rna_seq_utils/replace_N_fusion_vcf.py
+++ rna_seq_utils/replace_N_fusion_vcf.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
 
-# For fusion vcf, replace the "N" base in ALT from REF
+"""
+Title: replace_N_fusion_vcf.py
+Author: Jiaan Yu
+
+For fusion vcf, replace the "N" base in ALT from REF
+
+Example:
+'replace_N_fusion_vcf.py'
+  --input [File, vcf file from bcftools +fill-from-fasta]
+  > [String, output vcf]
+
+Example to run bcftools
+bcftools +fill-from-fasta
+  [File, vcf file from MegaFusion.py]
+  -- 
+  --column 'REF'
+  --fasta [File, reference Fasta file]
+  > 'output.fill.vcf'
+
+"""
 
 import argparse
 parser = argparse.ArgumentParser()
